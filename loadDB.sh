493667cadcb2c17a92e3252f450f985d6cdad608
Rob Court <rcourt@ed.ac.uk>
2021-04-13 11:29:34 +0100

    updating for new DB

--- loadDB.sh
+++ loadDB.sh
@@ -2,31 +2,24 @@
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 
-echo 'Allow new neo4j2owl plugin to make changes..'
-echo 'dbms.security.procedures.unrestricted=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
-#echo 'dbms.security.procedures.whitelist=ebi.spot.neo4j2owl.*,apoc.*' >> ${NEOSERCONF}
+echo 'Allow new plugin to make changes..'
+echo 'dbms.security.procedures.unrestricted=apoc.*,gds.*' >> ${NEOSERCONF}
 
 if [ -n "${BACKUPFILE}" ]; then
-  if [ ! -d /data/databases/graph.db ]; then
-    if [ ! -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from archive backup'
-      cd /opt/VFB/backup/
-      rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
-      wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
-      tar -xzvf ${BACKUPFILE}.tar.gz
-      mkdir -p /backup/DB-RESTORE.db/
-      find /opt/VFB/backup/ -name 'DB-RESTORE.db' -exec cp -vr "{}" /backup/ +
-      rm -rf /opt/VFB/backup/*
-      cd -
-    fi
-    if [ -d /backup/DB-RESTORE.db ]; then
-      echo 'Resore KB from given backup'
-      /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/DB-RESTORE.db --force=true
-    fi
+  if [ ! -d /data/databases/neo4j ]; then
+    echo 'Resore KB from archive backup'
+    cd /opt/VFB/backup/
+    rm /opt/VFB/backup/${BACKUPFILE}.tar.gz
+    wget http://data.virtualflybrain.org/archive/${BACKUPFILE}.tar.gz 
+    tar -xzvf ${BACKUPFILE}.tar.gz
+    mkdir -p /var/lib/neo4j/data/databases/
+    neo4j-admin restore --from /opt/VFB/backup/neo4j/neo4j --force
+    rm -rf /opt/VFB/backup/*
+    cd -
   fi
 fi
 
-echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
+echo -e '\nSTARTING VFB DB SERVER\n' >> /var/lib/neo4j/logs/query.log
 
 #Output the query log to docker log:
 tail -f /var/lib/neo4j/logs/query.log >/proc/1/fd/1 &
